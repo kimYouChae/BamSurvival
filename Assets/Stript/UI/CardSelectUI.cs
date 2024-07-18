852c8d009b8733298368e4a436976ffdc28d1cc5
김지현 <kjhstar2486@naver.com>
2024-07-19 00:33:49 +0900

    0718 : card Select ui 클릭이벤트 추가

--- Assets/Stript/UI/CardSelectUI.cs
+++ Assets/Stript/UI/CardSelectUI.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillCardSelect : MonoBehaviour
+public class CardSelectUI : MonoBehaviour
 {
     [Header("===CardSelectTool===")]
     [SerializeField]
     private GameObject _cardSelectUi;       // 카드 선택ui 부모 
     [SerializeField]
     private GameObject[] _cardSelectCardUi;     // 카드 선택 ui의 스킬카드 
+    [SerializeField]
+    private int _currCardIndex;
 
-    private void F_CardStacking() 
+    private void F_CardStacking()
     {
         // ##TODO : 카드 슬라이딩 
     }
 
+    // card idx 받아오기
+    public void F_SetCardIndex(int v_idx) 
+    {
+        _currCardIndex = v_idx;
+
+        Debug.Log(_currCardIndex);
+    }
 }
---
+++ Assets/Stript/UI/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [SerializeField]
+    private CardSelectUI _cardSelectUI;
+
+    // 프로퍼티
+    public CardSelectUI cardSelectUi => _cardSelectUI;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
